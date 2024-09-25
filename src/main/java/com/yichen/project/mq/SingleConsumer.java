00c749bdd5141ed794685f05e91079e9decccdb4
mojiez <937691466@qq.com>
2024-09-25 22:24:29 +0800

    添加了两个消息队列的案例 （单消费单生产 和 direct交换机）

---
+++ src/main/java/com/yichen/project/mq/SingleConsumer.java
@@ -0,0 +1,35 @@
+package com.yichen.project.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class SingleConsumer {
+
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        // 创建连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        // 创建队列 参数： 1. 消息队列名称 2. 消息队列重启后，信息是否丢失 3. 是否只允许当前这个创建消息队列的连接操作消息队列 4. 没有人用队列，是否要删除队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        // 定义了如何处理消息
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        // 消费消息，会持续阻塞
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+
+}
